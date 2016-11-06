afd2aaa5f37b0b8e9a9b955a33b1b49185ed7843
Tammen Bruccoleri <tammen.bruccoleri@gmail.com>
2016-11-06 13:20:08 -0800

    updating project with methods

---
+++ library/src/main/java/io/tammen/pulchra/ArrowDrawable.java
@@ -0,0 +1,12 @@
+package io.tammen.pulchra;
+
+/**
+ * Created by Tammen Bruccoleri on 11/6/16.
+ */
+
+public class ArrowDrawable {
+
+    public String drawResult(String result) {
+        return result;
+    }
+}
