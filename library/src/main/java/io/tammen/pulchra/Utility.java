2d582fa9350bc226abbb5fc29eb2a6a236bb6b72
Tammen Bruccoleri <tammen.bruccoleri@gmail.com>
2016-11-07 11:22:47 -0800

    Adding demo method

---
+++ library/src/main/java/io/tammen/pulchra/Utility.java
@@ -0,0 +1,12 @@
+package io.tammen.pulchra;
+
+/**
+ * Created by tammenbruccoleri on 11/7/16.
+ */
+
+public class Utility {
+
+    public String getResult(String value){
+        return value;
+    }
+}
